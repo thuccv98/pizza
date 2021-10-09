b4381a16f660b59e7b6ebb62279ee0cbc352a1db
thuccv98 <thuccv98@gmail.com>
2021-10-09 09:57:58 +0700

    Create products component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyle';
 import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData } from './data/productData';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Chose your favorite" data={productData} />
     </Router>
   );
 }
